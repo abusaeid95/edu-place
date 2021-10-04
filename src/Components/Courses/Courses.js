e24d0ef04029efa66baeaf39544cd4559edceaa3
Abu Saeid <abusaeid95@gmail.com>
2021-10-04 23:35:49 +0600

    finish

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -10,13 +10,16 @@ const Courses = () => {
         .then(data => setAuthors(data))
     },[])
     return (
-        <div class="row row-cols-1 row-cols-md-3 g-4 my-4">
+            <div class="row row-cols-1 row-cols-md-3 g-4 my-4">
             
             {
                 authors.map(authorr =><CourseCard authorr={authorr}>
                 </CourseCard>)
             }
+            
+        <button className="mx-auto subs-btn">Load More</button>
         </div>
+
     );
 };
 
