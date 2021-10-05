71fabe722a38701e13d8c7d8b68cd8f9d691d4b9
Abu Saeid <abusaeid95@gmail.com>
2021-10-05 12:21:58 +0600

    search box

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,15 +3,15 @@ import './SerachBar.css'
 
 const SearchBar = () => {
     return (
-        <div className="d-flex justify-content-between">
-                <dir>
+        <div className="src-area d-flex justify-content-between">
+            <div className="col">
                 <h2>All <span className="src-text text-align-center justify-content-center">Courses</span> of Edule</h2>
-                </dir>
-                <div className="col-6 mx-auto d-flex">
-                <label for="inputlg"></label>
-                <input class="form-control input-lg" id="inputlg" type="text" />
-                <button className="src-btn">Search</button>
+            </div>
+            <div className="col mx-auto d-flex">
+                <div class="box"><i class="fa fa-search" aria-hidden="true"></i>
+                    <input type="text" name="" />
                 </div>
+            </div>
         </div>
     );
 };
