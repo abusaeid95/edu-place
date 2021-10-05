61e0df624715e2f9a1c6ec9f156efdd57ead071c
Abu Saeid <abusaeid95@gmail.com>
2021-10-05 20:19:40 +0600

    searching add

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,23 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import CourseCard from '../CourseCard/CourseCard';
+import './Courses.css'
 
 const Courses = () => {
     const [authors, setAuthors] = useState([])
-    console.log(authors)
-    useEffect(()=>{
+    const [serachDisplay, setSearchDisplay] = useState([])
+    useEffect(() => {
         fetch('./fake.json')
-        .then(res => res.json())
-        .then(data => setAuthors(data))
-    },[])
+            .then(res => res.json())
+            .then(data =>{setSearchDisplay(data); setAuthors(data)})
+            
+    }, [])
+    const searchHandler = event => {
+        const searchText = event.target.value;
+        const searchAuthors = authors.filter(author => author.title.toLowerCase().includes(searchText.toLowerCase()))
+        setSearchDisplay(searchAuthors);
+    }
     return (
+        <div>
+            <div className="src-area d-flex justify-content-between">
+                <div className="col">
+                    <h2>All <span className="src-text text-align-center justify-content-center">Courses</span> of Edule</h2>
+                </div>
+                <div className="col mx-auto d-flex">
+                    <div class="box"><i class="fa fa-search" aria-hidden="true"></i>
+                        <input onChange={searchHandler} type="text" name="" />
+                    </div>
+                </div>
+            </div>
             <div class="row row-cols-1 row-cols-md-3 g-4 my-4">
+
+                {
+                    serachDisplay.map(authorr => <CourseCard authorr={authorr}>
+                    </CourseCard>)
+                }
+                
+            </div>
+            <div className="col text-center">
+            <button className=" text-center subss-btn">Load More</button>
+            </div>
             
-            {
-                authors.map(authorr =><CourseCard authorr={authorr}>
-                </CourseCard>)
-            }
-            
-        <button className="mx-auto subs-btn">Load More</button>
         </div>
 
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Courses from '../Courses/Courses';
-import SearchBar from '../SearchBar/SearchBar';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className="home container">
             <Banner></Banner>
-            <SearchBar></SearchBar>
             <Courses></Courses>
         </div>
     );
